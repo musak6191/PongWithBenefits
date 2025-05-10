f1883074574582db3e90ef35234d1a35b3498dcd
musak6191 <musa.k123@outlook.de>
2025-05-10 10:26:37 +0200

    RatPowerup weitergearbeitet
    Arbeite daran, dass die Ratte von der Seite angefahren werden kann

--- PongWithBenefits/Assets/Scripts/RatController.cs
+++ PongWithBenefits/Assets/Scripts/RatController.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RatController : MonoBehaviour
 {
     GameObject PlayerOne;
     GameObject PlayerTwo;
+    BoxCollider2D PlayerOneBoxCollider;
+    BoxCollider2D PlayerTwoBoxCollider;
 
     [SerializeField] float Speed;
+
+    bool isGettingRanOver;
+    float RanOverDistance = 1;
+    [SerializeField] LayerMask Player;
     // Start is called before the first frame update
     void Start()
     {
         PlayerOne = GameObject.FindWithTag("PlayerOne");
         PlayerTwo = GameObject.FindWithTag("PlayerTwo");
+        PlayerOneBoxCollider = PlayerOne.GetComponent<BoxCollider2D>();
+        PlayerTwoBoxCollider = PlayerOne.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, PlayerOne.transform.position, Time.deltaTime * Speed);
+
+        // Schauen ob die Ratte von der Seite von Pong getroffen wird
+        isGettingRanOver = Physics2D.Raycast(transform.position, Vector2.up, RanOverDistance, Player);
+
+        if ( isGettingRanOver)
+        {
+            Debug.Log("Spieler getroffen!");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -33,5 +50,11 @@ public class RatController : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * RanOverDistance);
     }
 }
