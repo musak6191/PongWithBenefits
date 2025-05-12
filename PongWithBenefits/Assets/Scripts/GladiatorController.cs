64d74fe0ec8474bdb5ddc7507a58ff1da83f943a
musak6191 <musa.k123@outlook.de>
2025-05-12 19:10:17 +0200

    Gladiator PowerupV2
    - Punktesystem drinnen
    - Spiel stoppt wenn zuende gefechtet wurde
    
    - Es wird nicht zur MainLevel geladen und die Punkte müssen dann noch übertragen werden

--- PongWithBenefits/Assets/Scripts/GladiatorController.cs
+++ PongWithBenefits/Assets/Scripts/GladiatorController.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GladiatorController : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI PointUI;
+    [SerializeField] TextMeshProUGUI GameOverUI;
     Rigidbody2D PlayerRigid;
+    [SerializeField] GameObject Player1;
+    [SerializeField] GameObject Player2;
+    SpriteRenderer PlayerRenderer;
+
     [SerializeField] bool isPlayerOne;
     [SerializeField] float MoveSpeed;
     [SerializeField] float RotationSpeed;
     [SerializeField] float RotationMaxSpeed;
 
+    public int PointsToWin = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerRigid = GetComponent<Rigidbody2D>();
+        StartCoroutine(MakePointsLessValuable());
+        PlayerRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float t = Time.deltaTime * 0.05f;
         // Schneller über Zeit
-            Mathf.Lerp(RotationSpeed, RotationMaxSpeed, Time.deltaTime);
+        RotationSpeed = Mathf.Lerp(RotationSpeed, RotationMaxSpeed, t);
 
         // drehen
         transform.Rotate(0, 0, RotationSpeed * Time.deltaTime);
@@ -51,7 +64,40 @@ public class GladiatorController : MonoBehaviour
     {
         if (collision.CompareTag("Projectile"))
         {
-            Destroy(gameObject);
+            PlayerRenderer.color = Color.black;
+            StartCoroutine(GladiatorOver());
         }
+    }
+
+    IEnumerator MakePointsLessValuable()
+    {
+        PointUI.text = "Points to win : " + PointsToWin;
+
+        yield return new WaitForSeconds(3);
+        PointsToWin = 5;
+        PointUI.text = "Points to win : " + PointsToWin;
+
+        yield return new WaitForSeconds(7);
+        PointsToWin = 3;
+        PointUI.text = "Points to win : " + PointsToWin;
+    }
+
+    IEnumerator GladiatorOver()
+    {
+        Time.timeScale = 0;
+        GameOverUI.gameObject.SetActive(true);
+        PointUI.gameObject.SetActive(false);
+        if (Player1 == null)
+        {
+            GameOverUI.text = "Player 2 wins " + PointsToWin + " points !";
+        }
+        else
+        {
+            GameOverUI.text = "Player 1 wins " + PointsToWin + " points !";
+        }
+
+        yield return new WaitForSeconds(3);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
